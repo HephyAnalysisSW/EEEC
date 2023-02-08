fc32e4f22ae39e612e2303e1b58f4b854be5ddec
Dennis Schwarz <dennis.schwarz@cern.ch>
2023-02-08 13:56:47 +0100

    include uncert in csv

--- csv/convertRootToCSV.sh
+++ csv/convertRootToCSV.sh
@@ -6,8 +6,20 @@
 # python convertRootToCSV.py --file=EEEC_1000_1725_804.root --reduce --mode=centers
 
 
-python convertRootToCSV.py --file=EEEC_1000_1725_754.root --reduce --mode=centers --round
+# python convertRootToCSV.py --file=EEEC_1000_1725_754.root --reduce --mode=centers --round
+# python convertRootToCSV.py --file=EEEC_1000_1725_804.root --reduce --mode=centers --round
+# python convertRootToCSV.py --file=EEEC_1000_1725_854.root --reduce --mode=centers --round
+# python convertRootToCSV.py --file=EEEC_1000_1825_754.root --reduce --mode=centers --round
+# python convertRootToCSV.py --file=EEEC_1000_1825_854.root --reduce --mode=centers --round
+
+# python convertRootToCSV.py --file=EEEC_1500_1725_754.root --reduce --mode=centers --round
+# python convertRootToCSV.py --file=EEEC_1500_1725_854.root --reduce --mode=centers --round
+
+
+python convertRootToCSV.py --file=EEEC_1000_1625_754.root --reduce --mode=centers --round
+python convertRootToCSV.py --file=EEEC_1000_1625_854.root --reduce --mode=centers --round
 python convertRootToCSV.py --file=EEEC_1000_1725_804.root --reduce --mode=centers --round
-python convertRootToCSV.py --file=EEEC_1000_1725_854.root --reduce --mode=centers --round
-python convertRootToCSV.py --file=EEEC_1000_1825_754.root --reduce --mode=centers --round
-python convertRootToCSV.py --file=EEEC_1000_1825_854.root --reduce --mode=centers --round
+python convertRootToCSV.py --file=EEEC_1250_1725_804.root --reduce --mode=centers --round
+python convertRootToCSV.py --file=EEEC_1500_1625_804.root --reduce --mode=centers --round
+python convertRootToCSV.py --file=EEEC_1500_1725_804.root --reduce --mode=centers --round
+python convertRootToCSV.py --file=EEEC_1500_1825_804.root --reduce --mode=centers --round
