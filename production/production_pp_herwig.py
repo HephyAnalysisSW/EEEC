f2c055076372d56e7ce9d5f4a0563018be553791
Dennis Schwarz <dennis.schwarz@cern.ch>
2023-07-14 17:36:36 +0200

    herwig test

---
+++ production/production_pp_herwig.py
@@ -0,0 +1,151 @@
+# Auto generated configuration file
+# using:
+# Revision: 1.19
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v
+# with command line options: Configuration/GenProduction/python/EXO-RunIIFall18GS-02638-fragment.py --python_filename EXO-RunIIFall18GS-02638_1_cfg.py --eventcontent RECOSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN --fileout file:EXO-RunIIFall18GS-02638.root --conditions 102X_upgrade2018_realistic_v11 --beamspot Realistic25ns13TeVEarly2018Collision --customise_commands process.source.numberEventsInLuminosityBlock=cms.untracked.uint32(100) --step GEN --geometry DB:Extended --era Run2_2018 --no_exec --mc -n 219
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+
+from Configuration.Generator.Herwig7Settings.Herwig7CH3TuneSettings_cfi import herwig7CH3SettingsBlock
+from Configuration.Generator.Herwig7Settings.Herwig7StableParticlesForDetector_cfi import *
+# from Configuration.Generator.Herwig7Settings.Herwig7_7p1SettingsFor7p2_cfi import *
+
+process = cms.Process('GEN',eras.Run2_2018)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic25ns13TeVEarly2018Collision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+import os
+maxEvents = int(os.environ["MAXEVENTS"])
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(maxEvents)
+)
+
+
+MT  = float(os.environ["MT"])
+MW  = float(os.environ["MW"])
+COM = float(os.environ["COM"])
+HADSWITCH = str(os.environ["HADSWITCH"])
+MPISWITCH = str(os.environ["MPISWITCH"])
+PTMIN = float(os.environ["PTMIN"])
+PTMAX = float(os.environ["PTMAX"])
+
+if os.environ.has_key('PRODUCTION_TMP_FILE'):
+    PRODUCTION_TMP_FILE = os.environ['PRODUCTION_TMP_FILE']
+else:
+    PRODUCTION_TMP_FILE = "PRODUCTION_TMP_FILE.root"
+
+print("Will process %i pp events for c.o.m %3.2f, mT = %3.2f, mW=%3.2f, %3.2f < pT < %3.2f, MPI: %s, Hadronization: %s"%(maxEvents, COM, MT, MW, PTMIN, PTMAX, MPISWITCH, HADSWITCH))
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+## Production Info
+#process.configurationMetadata = cms.untracked.PSet(
+#    annotation = cms.untracked.string('Configuration/GenProduction/python/EXO-RunIIFall18GS-02638-fragment.py nevts:219'),
+#    name = cms.untracked.string('Applications'),
+#    version = cms.untracked.string('$Revision: 1.19 $')
+#)
+
+# Output definition
+
+process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    ),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('file:'+PRODUCTION_TMP_FILE),
+    outputCommands = process.RECOSIMEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '102X_upgrade2018_realistic_v11', '')
+
+process.load("Configuration.StandardSequences.SimulationRandomNumberGeneratorSeeds_cff")
+process.RandomNumberGeneratorService.generator = process.RandomNumberGeneratorService.generator.clone()
+
+import random
+process.RandomNumberGeneratorService.generator.initialSeed = cms.untracked.uint32(random.randint(0,10**9))
+
+#############################################
+process.generator = cms.EDFilter("Herwig7GeneratorFilter",
+    herwig7CH3SettingsBlock,
+    herwig7StableParticlesForDetectorBlock,
+    # herwig7p1SettingsFor7p2Block,
+    productionParameters = cms.vstring(
+        'read snippets/PPCollider.in',
+        'cd /Herwig/MatrixElements/',
+        'insert SubProcess:MatrixElements[0] MEHeavyQuark',
+        'cd /',
+    ),
+    parameterSets = cms.vstring('productionParameters',
+                                'herwig7CH3PDF',
+                                'herwig7CH3AlphaS',
+                                'herwig7CH3MPISettings',
+                                'herwig7StableParticlesForDetector'),
+                                # 'hw_7p1SettingsFor7p2'),
+    configFiles = cms.vstring(),
+    crossSection = cms.untracked.double(1363000000),
+    dataLocation = cms.string('${HERWIGPATH:-6}'),
+    eventHandlers = cms.string('/Herwig/EventHandlers'),
+    filterEfficiency = cms.untracked.double(1.0),
+    generatorModule = cms.string('/Herwig/Generators/EventGenerator'),
+    repository = cms.string('${HERWIGPATH}/HerwigDefaults.rpo'),
+    run = cms.string('InterfaceMatchboxTest'),
+    runModeList = cms.untracked.string("read,run"),
+)
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.endjob_step,process.RECOSIMoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# End of customisation functions
+
+# Customisation from command line
+
+process.source.numberEventsInLuminosityBlock=cms.untracked.uint32(100)
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
---
+++ production/production_pp_herwig.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export SCRAM_ARCH=slc6_amd64_gcc700
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+eval `scram runtime -sh`
+
+uuid=$(uuidgen)
+
+export WORKDIR=/scratch-cbe/users/$USER/tmp/
+mkdir -p $WORKDIR
+export OUTPUTDIR=/groups/hephy/cms/$USER/EEEC/output/
+mkdir -p $OUTPUTDIR
+
+export PRODUCTION_TMP_FILE=$WORKDIR/$uuid.root
+
+export POSTFIX=$uuid
+export MAXEVENTS=$1
+export COM=$2
+export MT=$3
+export MW=$4
+export PTMIN=$5
+export PTMAX=$6
+export MPISWITCH=$7
+export HADSWITCH=$8
+
+
+
+# Run the cmsRun
+#ipython -i $PWD/production.py || exit $? ;
+cmsRun -e $PWD/production_pp_herwig.py || exit $? ;
+python genPostProcessing_pp.py --input $PRODUCTION_TMP_FILE --output $OUTPUTDIR/output_${COM}_${MT}_${MW}_HARDPT${PTMIN}to${PTMAX}_MPI${MPISWITCH}_HAD${HADSWITCH}_HERWIG_$POSTFIX
+
+rm $PRODUCTION_TMP_FILE
